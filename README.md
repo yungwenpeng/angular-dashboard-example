9ab075f75a2cddad903b1a8fd2acc41660b131d4
yungwenpeng <yungwenpeng@gmail.com>
2021-12-28 18:40:13 +0800

    Create a new dashboard component & install dependence library
    Commands :
         ng generate component dashboard
         npm install chart.js
         ng add @angular/material

--- README.md
+++ README.md
@@ -25,3 +25,7 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+  
+## Others :  
+## Refer to [Line Chart](https://www.chartjs.org/docs/latest/samples/line/line.html#line-chart)  
+## Refer to [angular material](https://material.angular.io/components/categories)  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
